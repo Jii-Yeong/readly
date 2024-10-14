963be95960991324f2892711064b7768ba41832a
Jii-Yeong <tj5134@naver.com>
2024-10-14 15:09:49 +0900

    feat: 메인 컬러 설정 및 헤더 임시 제작

---
+++ src/components/ui/header/DefaultHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DefaultHeader() {
+  return (
+    <div className="bg-main w-full h-[56px] px-[8px]">
+      <h1 className="text-white text-[32px] font-bold">READLY</h1>
+    </div>
+  )
+}
