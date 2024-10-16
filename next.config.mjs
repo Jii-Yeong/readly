51031c51d44d8150ddf447e4c4179ad220a1de99
Jii-Yeong <tj5134@naver.com>
2024-10-16 15:10:11 +0900

    feat: 구글 로그인 아이디 환경변수 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
